8deb9bf188509c2caeaf4f358d431311059e0231
bambademe <cheikhbambademe@gmail.com>
2022-12-05 23:19:06 +0000

    creating components and config routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AccountsComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
